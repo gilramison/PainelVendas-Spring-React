65665bf6fc135e2d51dcbc56ab6cf01fe757b372
GilRamison <gilramison@gmail.com>
2022-09-05 20:16:27 -0300

    project created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import NotificationButton from "./components/NotificationButton"
+
+function App() {
+ 
+  return(
+    <>
+    <h1> Olá Mundo! </h1>
+    <NotificationButton/>
+
+    <NotificationButton/>
+
+    <NotificationButton/>
+
+    <NotificationButton/>
+    </>
+  )
+ 
+}
+
+export default App
